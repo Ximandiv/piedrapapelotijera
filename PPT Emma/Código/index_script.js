19e38ba6dabe0a0556a37da9978feb623077dc7d
RainAngel <119233001+Ximandiv@users.noreply.github.com>
2023-03-31 16:26:43 -0500

    Se cambia para estar a la fecha

--- PPT Emma/Código/index_script.js
+++ PPT Emma/Código/index_script.js
@@ -1,4 +1,4 @@
-import Swal from 'sweetalert2/dist/sweetalert2.js';
+"use strict";
 
 //Se capturan elementos del DOM para opciones
 //Piedra papel y tijera
@@ -12,8 +12,8 @@ let botonTijera = document.querySelector(".tijera");
 let manoUsuario = document.querySelector(".mano-usuario");
 let manoComputadora = document.querySelector(".mano-computadora");
 
-let puntajeUsuario = document.querySelector(".puntaje-usuario");
-let puntajeComputadora = document.querySelector(".puntaje-computadora");
+let puntajeUsuario = document.querySelector(".puntaje-usuario p");
+let puntajeComputadora = document.querySelector(".puntaje-computadora p");
 
 let resultado = document.querySelector(".resultado");
 
@@ -23,40 +23,205 @@ let eleccionUsuario = "";
 let eleccionComputadora = "";
 
 let contadorUsuario = 0;
-let contadorComputador = 0;
+let contadorComputadora = 0;
+
+function unableToClick(){
+    document.querySelector(".piedra").disabled = true;
+    document.querySelector(".papel").disabled = true;
+    document.querySelector(".tijera").disabled = true;
+}
+function ableToClick(){
+    document.querySelector(".piedra").disabled = false;
+    document.querySelector(".papel").disabled = false;
+    document.querySelector(".tijera").disabled = false;
+}
 
 //ALERTA
 
 const swalInicio = () =>{
-    Swal.fire(
-        'Juguemos Piedra Papel o Tijera!',
-        'You clicked the button!',
-        'success'
-    )
+    Swal.fire({
+        icon: 'question',
+        title: 'Juguemos!',
+        text: 'El primero a los 3 puntos en Piedra Papel o Tijera gana',
+        confirmButtonText: 'Jugar',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false
+    })
 }
-
 swalInicio();
 
 const swalGanador = () =>{
     Swal.fire({
         icon: 'success',
-        title: 'Oh no!',
-        text: 'Me has ganado! Felicidades.'
+        title: 'Me has ganado!',
+        text: 'Felicidades, me has ganado.',
+        confirmButtonText: 'Reiniciar',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false
     })
 }
-
-swalGanador();
-
 const swalPerdedor = () =>{
     Swal.fire({
         icon: 'error',
-        title: 'Hehe!',
-        text: 'Buen intento pero, gané!'
+        title: 'Yo gano!',
+        text: 'A la próxima quizás me ganes.',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false
     })
 }
 
-swalPerdedor();
+const limpiarMarcador = () => {
+    contadorUsuario = 0;
+    contadorComputadora = 0;
+    puntajeUsuario.textContent = contadorUsuario;
+    puntajeComputadora.textContent = contadorComputadora;
+}
 
-function LimpiarMarcador(){
+const ganadores = (puntajeUsuario, puntajeComputadora) =>{
+    console.log("Usuario:" +puntajeUsuario.textContent ,"Computador:" +puntajeComputadora.textContent, "Se muestra resultado");
+    if(puntajeUsuario.textContent == 3 && puntajeUsuario.textContent > puntajeComputadora.textContent){
+        console.log("Usuario ganador");
+        setTimeout(swalGanador, 1000);
+        setTimeout(limpiarMarcador, 1200);
+        setTimeout(reinicio, 1500);
+    }
+    else if(puntajeComputadora.textContent == 3 && puntajeUsuario.textContent < puntajeComputadora.textContent){
+        console.log("Computadora ganador");
+        setTimeout(swalPerdedor, 1000);
+        setTimeout(limpiarMarcador, 1200);
+        setTimeout(reinicio, 1500);
+    }
+}
+const reinicio = () =>{
+    resultado.textContent = "Seleccione una opción";
+    manoComputadora.src = "../assets/piedra_computadora.png";
+    manoUsuario.src = "../assets/piedra_ada.png";
+}
 
+const resul = () =>{
+    if(eleccionUsuario == "piedra"){
+        if(eleccionComputadora == "piedra"){
+            resultado.textContent = "Empatamos!";
+            ableToClick();
+        }
+        if(eleccionComputadora == "papel"){
+            resultado.textContent = "Perdiste, hehe.";
+            contadorComputadora++;
+            puntajeComputadora.textContent = contadorComputadora;
+            ganadores(puntajeUsuario, puntajeComputadora);
+            ableToClick();
+        }
+        if(eleccionComputadora == "tijera"){
+            resultado.textContent = "Me has ganado! Bien...";
+            contadorUsuario++;
+            puntajeUsuario.textContent = contadorUsuario;
+            ganadores(puntajeUsuario, puntajeComputadora);
+            ableToClick();
+        }
+    }
+    else if(eleccionUsuario == "papel"){
+        if(eleccionComputadora == "piedra"){
+            resultado.textContent = "Me has ganado! Bien...";
+            contadorUsuario++;
+            puntajeUsuario.textContent = contadorUsuario;
+            ganadores(puntajeUsuario, puntajeComputadora);
+            ableToClick();
+        }
+        if(eleccionComputadora == "papel"){
+            resultado.textContent = "Empatamos!";
+            ableToClick();
+        }
+        if(eleccionComputadora == "tijera"){
+            resultado.textContent = "Perdiste, hehe.";
+            contadorComputadora++;
+            puntajeComputadora.textContent = contadorComputadora;
+            ganadores(puntajeUsuario, puntajeComputadora);
+            ableToClick();
+        }
+    }
+    else if(eleccionUsuario == "tijera"){
+        if(eleccionComputadora == "piedra"){
+            resultado.textContent = "Perdiste, hehe.";
+            contadorComputadora++;
+            puntajeComputadora.textContent = contadorComputadora;
+            ganadores(puntajeUsuario, puntajeComputadora);
+            ableToClick();
+        }
+        if(eleccionComputadora == "papel"){
+            resultado.textContent = "Me has ganado! Bien...";
+            contadorUsuario++;
+            puntajeUsuario.textContent = contadorUsuario;
+            ganadores(puntajeUsuario, puntajeComputadora);   
+            ableToClick();
+        }
+        if(eleccionComputadora == "tijera"){
+            resultado.textContent = "Empatamos!";
+            ableToClick();
+        }
+    }
+}
+
+const IACOMPUTADORA = () =>{
+    let RNG = Math.floor(Math.random()*3);
+
+    if (RNG == 0){
+        manoComputadora.src = "../assets/piedra_computadora.png";
+        eleccionComputadora = "piedra";
+    } 
+    else if (RNG == 1){
+        manoComputadora.src = "../assets/papel_computadora.png";
+        eleccionComputadora = "papel";
+    } 
+    else{
+        manoComputadora.src = "../assets/tijera_computadora.png";
+        eleccionComputadora = "tijera";
+    }
+}
+
+botonPiedra.onclick = () =>{
+    unableToClick();
+    manoUsuario.src = "../assets/piedra_ada.png";
+    manoComputadora.src = "../assets/piedra_computadora.png";
+    resultado.textContent = "...";
+    tablero.classList.add("jugando");
+    setTimeout(() => {
+        eleccionUsuario = "piedra";
+        manoUsuario.src = "../assets/piedra_ada.png";
+        IACOMPUTADORA();
+        tablero.classList.remove("jugando");
+        resul();
+    }, 2000);
+}
+
+botonTijera.onclick = () =>{
+    unableToClick();
+    manoUsuario.src = "../assets/piedra_ada.png";
+    manoComputadora.src = "../assets/piedra_computadora.png";
+    resultado.textContent = "...";
+    tablero.classList.add("jugando");
+    setTimeout(() => {
+        eleccionUsuario = "tijera";
+        manoUsuario.src = "../assets/tijera_ada.png";
+        IACOMPUTADORA();
+        tablero.classList.remove("jugando");
+        resul();
+    }, 2000);
+}
+
+botonPapel.onclick = () =>{
+    unableToClick();
+    manoUsuario.src = "../assets/piedra_ada.png";
+    manoComputadora.src = "../assets/piedra_computadora.png";
+    resultado.textContent = "...";
+    tablero.classList.add("jugando");
+    setTimeout(() => {
+        eleccionUsuario = "papel";
+        manoUsuario.src = "../assets/papel_ada.png";
+        IACOMPUTADORA();
+        tablero.classList.remove("jugando");
+        resul();
+    }, 2000);
 }
